a1059a906820f9135640ba42b45c96bf7462bc42
Сергей <nyos@rambler.ru>
2019-06-26 18:29:30 +0500

    no message

--- 1/index.php
+++ 1/index.php
@@ -4,8 +4,11 @@
 
 // \f\pa($_POST);
 
-if (isset($vv['now_mod']['no_cats']{1})) {
-    $vv['tpl_0body'] = \f\like_tpl('sh-no.cat', $vv['dir_module_tpl'], $vv['dir_site_tpl']);
-} else {
-    $vv['tpl_0body'] = \f\like_tpl('sh', $vv['dir_module_tpl'], $vv['dir_site_tpl']);
-}
+//if (isset($vv['now_mod']['no_cats']{1})) {
+//    $vv['tpl_0body'] = \f\like_tpl('sh-no.cat', $vv['dir_module_tpl'], $vv['dir_site_tpl']);
+//} else {
+    // $vv['body'] = \f\like_tpl('sh', $vv['dir_module_tpl'], $vv['dir_site_tpl']);
+//}
+    
+//echo 
+$vv['tpl_body'] = dir_site_module_nowlev_tpl.'body.htm';
